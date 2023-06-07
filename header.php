ecc77e5b5a98581bbf8a28d97679fff9b44da842
maxance13 <115090832+maxance13@users.noreply.github.com>
2023-06-07 11:28:55 +0100

    suite

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     </div>
     <nav>
         <ul>
-            <a href="#" class="NavPage"><li>Coach</li></a>
+            <a href="coach.php" class="NavPage"><li>Coach</li></a>
             <a href="#" class="NavPage"><li>Contact</li></a>
             <a href="#"><li><img src="Img/icons8-utilisateur-24.png" alt="User Page"></li></a>
         </ul>
