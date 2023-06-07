cb40fac4c0f6511af0a2dba2596e65e4f4dd785c
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2023-06-07 13:15:27 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <header>
     <div class="FitHome">
-        <a href="index.html">
+        <a href="index.php">
             <img src="Img/logo_startp-removebg-preview.png" alt="Logo FitHome" width="75px">
             <p class="Fit-Home">Fit-Home</p>
         </a>
     </div>
     <nav>
         <ul>
-            <a href="coach.php" class="NavPage"><li>Coach</li></a>
-            <a href="#" class="NavPage"><li>Contact</li></a>
+            <a href="#" class="NavPage"><li>Coach</li></a>
+            <a href="contact.php" class="NavPage"><li>Contact</li></a>
             <a href="#"><li><img src="Img/icons8-utilisateur-24.png" alt="User Page"></li></a>
         </ul>
     </nav>
