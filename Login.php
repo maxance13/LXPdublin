5ec5b371e825a718a2e51c918032fd183f1c4489
maxance13 <115090832+maxance13@users.noreply.github.com>
2023-06-11 21:29:15 +0100

    register login
    bug css login jsp

---
+++ Login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | FitHome</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="Img/logo_startp-removebg-preview - Copie.png">
+</head>
+<body>
+
+    <?php require('.\header.php') ?>
+
+<form class="form">
+    <p class="title">Login </p>
+    <p class="message">Sign in to your account. </p>
+        <div class="flex">
+        
+            
+    <label>
+        <input required="" placeholder="" type="email" class="input">
+        <span>Email</span>
+    </label> 
+        
+    <label>
+        <input required="" placeholder="" type="password" class="input">
+        <span>Password</span>
+    </label>
+    <button class="submit">Sign in</button>
+    <p class="signin">Doesn't have an account<a href="register.php">Sign up</a> </p>
+</form>
+
+<?php require('.\footer.php') ?>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | FitHome</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="Img/logo_startp-removebg-preview - Copie.png">
+</head>
+<body>
+
+    <?php require('.\header.php') ?>
+    
+    <form class="form">
+    <p class="title">Register </p>
+    <p class="message">Signup now and get full access to our app. </p>
+        <div class="flex">
+        <label>
+            <input required="" placeholder="" type="text" class="input">
+            <span>Firstname</span>
+        </label>
+
+        <label>
+            <input required="" placeholder="" type="text" class="input">
+            <span>Lastname</span>
+        </label>
+    </div>  
+            
+    <label>
+        <input required="" placeholder="" type="email" class="input">
+        <span>Email</span>
+    </label> 
+        
+    <label>
+        <input required="" placeholder="" type="password" class="input">
+        <span>Password</span>
+    </label>
+    <label>
+        <input required="" placeholder="" type="password" class="input">
+        <span>Confirm password</span>
+    </label>
+    <button class="submit">Submit</button>
+    <p class="signin">Already have an acount ? <a href="Login.php">Signin</a> </p>
+</form>
+
+<?php require('.\footer.php') ?>
+
+</body>
+</html>
