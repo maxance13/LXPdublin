06ca9b91c3cdec022ea4bf45adb30a5a35ab1fd6
maxance13 <115090832+maxance13@users.noreply.github.com>
2023-06-13 14:44:08 +0100

    Update coach.php

--- coach.php
+++ coach.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    $mysqlConnection = new PDO(
+        'mysql:host=localhost;dbname=fithome;charset=utf8','root','root'
+    );
+    ?>
     <title>Coach | FitHome</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="shortcut icon" href="Img/logo_startp-removebg-preview - Copie.png">
+    
 </head>
 <body>
 
